df7041da0af641a836883afaa3f49340b9d302f9
Julia Carolina <julicarolyna@gmail.com>
2026-05-20 08:59:38 -0300

    Add files via upload

---
+++ Ling_prog_2026/src/J_Manipuação_de_string/lowercase.java
@@ -0,0 +1,16 @@
+package J_Manipuação_de_string;
+
+public class lowercase {
+
+    public static void main(String[] args) {
+        //char Character.toLowerCase(char c);
+
+        String frase = "Amo meu namorado";
+        System.out.println(frase);
+
+        String fraseMinuscula = frase.toLowerCase();
+
+        System.out.println(fraseMinuscula);
+
+    }
+}
