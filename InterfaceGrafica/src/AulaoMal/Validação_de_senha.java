42f9fdf34d972255dcf03c3cb10a8e61fcf64a0c
Julia Carolina <julicarolyna@gmail.com>
2026-07-29 09:03:40 -0300

    Interface gráfica aula 1

---
+++ InterfaceGrafica/src/AulaoMal/Validação_de_senha.java
@@ -0,0 +1,12 @@
+package AulaoMal;
+
+public class Validação_de_senha {
+
+    public static void main(String[] args) {
+        Metodos_validacao metodo = new Metodos_validacao();
+
+        String email = metodo.ValidacaoEmail();
+        
+        String senha = metodo.ValidacaoSenha();
+    }
+}
